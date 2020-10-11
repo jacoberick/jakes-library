2d9647c6fcee162be33943728d637e1dcf877fef
jacoberick <jacobewhitedev@gmail.com>
2020-10-11 01:04:27 -0500

    library construction

--- src/components/library.js
+++ src/components/library.js
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Library = () => {
-  return <div id="libraryContainer"></div>;
+  const books = [
+    {
+      title: "Anthem",
+      author: "Ayn Rand",
+      cover: "anthem.jpg",
+      alt: "Anthem Book Cover",
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+      cover: "1984.jpg",
+      alt: "1984 Book Cover",
+    },
+    {
+      title: "The Gulag Archipelago",
+      author: "Aleksandr Solzhenitsyn",
+      cover: "gulag.jpg",
+      alt: "The Gulag Archipelago Book Cover",
+    },
+  ];
+
+  return (
+    <div id="libraryContainer">
+      <div id="bookCase">
+        {books.map((b, id) => (
+          <div key={id} className="book-container">
+            <img src={require(`../assets/${b.cover}`)} alt={b.alt} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Library;
