6935764944baf0e0e9de921a20b2ef0119981396
Collin Smith <collin.smith@iostudio.com>
2020-10-22 21:57:17 -0500

    Added new single book route logic to root component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import Gateway from "./components/gateway.js";
 import Nav from "./components/nav.js";
 import Library from "./components/library.js";
+import Book from "./components/book.js";
 
 function App() {
+  const [book, selectBook] = useState({});
+  const books = [
+    {
+      id: 1,
+      title: "Anthem",
+      author: "Ayn Rand",
+      cover: "anthem.jpg",
+      alt: "Anthem Book Cover",
+    },
+    {
+      id: 2,
+      title: "1984",
+      author: "George Orwell",
+      cover: "1984.jpg",
+      alt: "1984 Book Cover",
+    },
+    {
+      id: 3,
+      title: "The Gulag Archipelago",
+      author: "Aleksandr Solzhenitsyn",
+      cover: "gulag.jpg",
+      alt: "The Gulag Archipelago Book Cover",
+    },
+    {
+      id: 4,
+      title: "Fahrenheit 451",
+      author: "Ray Bradbury",
+      cover: "451.jpg",
+      alt: "Fahrenheit 451 Book Cover",
+    },
+    {
+      id: 5,
+      title: "The Giver",
+      author: "Louis Lowry",
+      cover: "giver.jpg",
+      alt: "The Giver Book Cover",
+    },
+  ];
+
   return (
     <Router>
       <div id="masterContainer">
@@ -14,9 +54,13 @@ function App() {
           <Route exact path="/">
             <Gateway />
           </Route>
+          <Route path="/library/:id">
+            <Nav />
+            <Book book={book} />
+          </Route>
           <Route path="/library">
             <Nav />
-            <Library />
+            <Library books={books} selectBook={selectBook} />
           </Route>
         </Switch>
       </div>
