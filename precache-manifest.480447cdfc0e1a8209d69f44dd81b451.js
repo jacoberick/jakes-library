ee8fee51fdf67cb1875b635aeee5339c77b92ab1
jacoberick <jacobewhitedev@gmail.com>
2020-11-11 20:33:26 -0600

    Updates

---
+++ precache-manifest.480447cdfc0e1a8209d69f44dd81b451.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebd25c93b8df4505b28a41a20e49f6f",
+    "url": "/jakes-library/index.html"
+  },
+  {
+    "revision": "a4c7e5de5b65f28b437f",
+    "url": "/jakes-library/static/css/main.38c59ac5.chunk.css"
+  },
+  {
+    "revision": "96f10e52cfc49a9b2571",
+    "url": "/jakes-library/static/js/2.3d478206.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jakes-library/static/js/2.3d478206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c7e5de5b65f28b437f",
+    "url": "/jakes-library/static/js/main.2bc828e9.chunk.js"
+  },
+  {
+    "revision": "08e2a8d072852b38e603",
+    "url": "/jakes-library/static/js/runtime-main.643e01da.js"
+  },
+  {
+    "revision": "f4a644a62679686ea884c95836d717c4",
+    "url": "/jakes-library/static/media/12.f4a644a6.jpg"
+  },
+  {
+    "revision": "bcd9007eaf1be3d070027b3f93ea547a",
+    "url": "/jakes-library/static/media/1984.bcd9007e.jpg"
+  },
+  {
+    "revision": "7250f0bab97202596ba4cab64c3fabf9",
+    "url": "/jakes-library/static/media/451.7250f0ba.jpg"
+  },
+  {
+    "revision": "ccb6a6b4e09a1ad135d44acf949ed752",
+    "url": "/jakes-library/static/media/aleks.ccb6a6b4.jpg"
+  },
+  {
+    "revision": "2c203d29aa577666795dd683b2aab52c",
+    "url": "/jakes-library/static/media/anthem.2c203d29.jpg"
+  },
+  {
+    "revision": "765cb53c4ed1a0d1fa287c10cc91baba",
+    "url": "/jakes-library/static/media/aynRand.765cb53c.jpg"
+  },
+  {
+    "revision": "bce156e6bd938c3b8443c5156368cf90",
+    "url": "/jakes-library/static/media/georgeOrwell.bce156e6.jpg"
+  },
+  {
+    "revision": "834cb0e3bf361b8f6fa4c58731858346",
+    "url": "/jakes-library/static/media/giver.834cb0e3.jpg"
+  },
+  {
+    "revision": "3e5b1eeea9012e3132fa1b56378f1ace",
+    "url": "/jakes-library/static/media/givingTree.3e5b1eee.jpg"
+  },
+  {
+    "revision": "f515c8928244a4141226dd85545acb47",
+    "url": "/jakes-library/static/media/gulag.f515c892.jpg"
+  },
+  {
+    "revision": "e605309c5820789a725f1a7f4fbd626c",
+    "url": "/jakes-library/static/media/hp.e605309c.jpg"
+  },
+  {
+    "revision": "068740f73a0561fd2257704e75e1cb17",
+    "url": "/jakes-library/static/media/lois.068740f7.jpg"
+  },
+  {
+    "revision": "77ec9369fc2c95f15e0fd44e9fbae38f",
+    "url": "/jakes-library/static/media/lord.77ec9369.jpg"
+  },
+  {
+    "revision": "2255a56c574303e42472e293a774caa9",
+    "url": "/jakes-library/static/media/ray.2255a56c.jpg"
+  },
+  {
+    "revision": "517f340279d76a8d5bfe258cef9a6274",
+    "url": "/jakes-library/static/media/shel.517f3402.jpg"
+  },
+  {
+    "revision": "0368bbf9b660a8159161adf3f8b183db",
+    "url": "/jakes-library/static/media/will.0368bbf9.jpg"
+  }
+]);
